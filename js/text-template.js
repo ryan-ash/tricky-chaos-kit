180f5e463077d65f4d57aeba9f01249554e97edc
Ryan Ash <scarletspark@gmail.com>
2019-09-04 03:58:02 +0300

    scripts matured a bit; adding web controller support & text template feature skeleton

--- js/text-template.js
+++ js/text-template.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
     var $body = $("body");
-    var feature_name = "purge";
+    var feature_name = "text-template"
+    var overlay_markup = `
+        <div class="OUTER_CLASS_NAME">
+            test
+        </div>
+    `;
+    overlay_markup = overlay_markup.replace('OUTER_CLASS_NAME', get_overlay_class().substring(1));
 
     if ($.cookie(feature_name)) {
         enable();
@@ -20,12 +26,21 @@ $(document).ready(function() {
     }
 
     function enable() {
+        $body.append(overlay_markup);
+
         $body.addClass(feature_name);
         $.cookie(feature_name, true);
     }
 
     function disable() {
+        console.log($body.find(get_overlay_class()));
+        $body.find(get_overlay_class()).remove();
+
         $body.removeClass(feature_name);
         $.removeCookie(feature_name);
+    }
+
+    function get_overlay_class() {
+        return ".overlay-" + feature_name;
     }
 });
