2e190a3d629d1927108255979c8db8b6530956fa
Ryan Ash <scarletspark@gmail.com>
2020-01-09 05:18:49 +0300

    form restyle, still in progress

--- js/text-template-markup.js
+++ js/text-template-markup.js
@@ -14,9 +14,30 @@ var overlay_markup = `
 `;
 
 var form_content = `
+    <div class="tc-form-controls">
+        <div class="tc-button-container">
+            <div tabindex="0" class="tc-toggle-preview tc-button fa fa-eye"></div>
+            <div tabindex="0" class="tc-clear-form tc-button fa fa-trash-o"></div>
+            <div tabindex="0" class="tc-save-draft tc-button fa fa-floppy-o"></div>
+            <div tabindex="0" class="tc-save-draft-complete tc-button tc-disabled fa fa-check"></div>
+            <div tabindex="0" class="tc-load-draft tc-button fa fa-arrow-down"></div>
+        </div>
+        <div class="tc-drafts">
+            <div class="tc-draft-right tc-draft-column">
+                <div class="tc-load-draft-form tc-disabled">
+                    <div class="tc-select-wrapper">
+                        <select class="tc-draft-list">
+                        </select>
+                    </div>
+                    <div tabindex="0" class="tc-load tc-button fa fa-upload"></div>
+                    <div tabindex="0" class="tc-delete tc-button fa fa-trash-o"></div>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="tc-title-wrapper"></div>
     <div tabindex="0" class="tc-add tc-button tc-add-title fa fa-plus"></div><br/>
-    <div class="tc-tag-selector tc-wide-input">
+    <div class="tc-tag-selector tc-wide-input tc-wrapper">
         <input type="text" class="tc-text-input tc-tag-selector-input" placeholder="#tags">
         <div tabindex="0" class="tc-reset tc-button fa fa-minus"></div>
         <div class="tc-tag-helper">
@@ -27,55 +48,31 @@ var form_content = `
             <div tabindex="0" class="tc-cancel tc-disabled tc-button fa fa-times"></div>
         </div>
     </div>
-    <div class="tc-preview-link tc-wide-input">
+    <div class="tc-preview-link tc-wide-input tc-wrapper">
         <input type="text" class="tc-text-input tc-preview-link-input" placeholder="Preview Link">
         <div tabindex="0" class="tc-reset tc-button fa fa-minus"></div>
     </div>
-    <div class="tc-text-wrapper">
+    <div class="tc-text-wrapper tc-wrapper">
         <div id="tc-text" class="tc-text"></div>
         <div tabindex="0" class="tc-reset tc-button fa fa-minus"></div>
     </div>
     <div class="tc-bottom-link-wrapper"></div>
     <div tabindex="0" class="tc-add tc-button tc-add-bottom-link fa fa-plus"></div><br/>
-    <div class="tc-ps-text tc-wide-input">
+    <div class="tc-ps-text tc-wide-input tc-wrapper">
         <input type="text" class="tc-text-input tc-ps-text-input" placeholder="PS">
         <div tabindex="0" class="tc-reset tc-button fa fa-minus"></div>
-    </div>
-    <div class="tc-form-footer">
-        <!--a href="#" class="tc-parse tc-wide-button tc-text-button">Parse</a-->
-        <a href="#" class="tc-clear-form tc-wide-button tc-text-button">Clear</a>
-        <div class="tc-drafts">
-            <div class="tc-draft-left tc-draft-column">
-                <a href="#" class="tc-save-draft tc-narrow-button tc-text-button">
-                    <span class="tc-save-draft-text">Save Draft</span>
-                    <span class="tc-save-draft-icon fa fa-floppy-o"></span>
-                </a>
-                <a href="#" class="tc-save-draft-complete tc-narrow-button tc-text-button tc-disabled fa fa-check"></a>
-            </div>
-            <div class="tc-draft-right tc-draft-column">
-                <a href="#" class="tc-load-draft tc-narrow-button tc-text-button">Load Draft</a>
-                <div class="tc-load-draft-form tc-disabled">
-                    <div class="tc-select-wrapper">
-                        <select class="tc-draft-list">
-                        </select>
-                    </div>
-                    <div tabindex="0" class="tc-load tc-button fa fa-upload"></div>
-                    <div tabindex="0" class="tc-delete tc-button fa fa-trash-o"></div>                
-                </div>
-            </div>
-        </div>
     </div>
 `;
 
 var title_markup = `
-    <div class="tc-title tc-wide-input">
+    <div class="tc-title tc-wide-input tc-wrapper">
         <input type="text" class="tc-text-input tc-title-input" placeholder="Title">
         <div tabindex="0" class="tc-remove tc-button fa fa-minus"></div>
     </div>
 `;
 
 var bottom_link_markup = `
-    <div class="tc-bottom-link">
+    <div class="tc-bottom-link tc-wrapper">
         <input type="text" placeholder="http://" class="tc-text-input tc-bottom-link-input tc-link">
         <input type="text" placeholder="Link Text" class="tc-text-input tc-bottom-link-input tc-link-text">
         <div tabindex="0" class="tc-remove tc-button fa fa-minus"></div>
